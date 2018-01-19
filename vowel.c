821070361590685e24555b6c1f032124daa66101
priya123dharshini <30829459+priya123dharshini@users.noreply.github.com>
2018-01-19 10:49:49 +0000

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+ int str[34],i,flag=0;
+ printf("enter a string");
+ scanf("%s",&str[i]);
+ while(str[i]!=" ")
+ {
+    if(str[i]=="a"||str[i]=="e"||str[i]=="i"||str[i]=="o"||str[i]=="u")
+    { 
+     flag=1;
+     i++;
+    printf("yes");
+    }
+    else{
+    printf("no");
+    }
+    
+    
+    
+   
+    
+  

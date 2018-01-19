912b1d873b87b21aeca63a6b056f4a3946493dcc
priya123dharshini <30829459+priya123dharshini@users.noreply.github.com>
2018-01-19 11:13:22 +0000

    Create perfectsquare.c

---
+++ perfectsquare.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+ int n1,n2,mul,s;
+ printf("enter number 1");
+ scanf("%d",&n1);
+ printf("enter number 2");
+ scanf("%d",&n2);
+ mul=n1*n2;
+ printf("the product of the 2 numbers is %d",mul);
+ s=sqrt(mul);
+ if(m==n1)
+ {
+   if(m==n2)
+   {
+     printf("the number is a perfect square");
+     }
+     }
+  else
+  {
+   printf("the number is not a perfect square");
+   }
+   }
+ 

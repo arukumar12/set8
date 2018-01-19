d1b5a1bfd6351fd17b11bcf89e8e207337477378
priya123dharshini <30829459+priya123dharshini@users.noreply.github.com>
2018-01-19 10:55:44 +0000

    Create multiple13.c

---
+++ multiple13.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+  int num;
+  printf("enter a number");
+  scanf("%d",&num);
+  while(num!=0)
+  {
+      do
+      {
+      m=num%13
+      }
+      if(m==0)
+      {
+       printf("multiple of 13");
+       }
+       else
+       {
+       printf("not a multiple of 13");
+       }
+       }
+       
+       }

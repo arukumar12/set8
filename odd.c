6ce1c382b233bdb1e86ab34e23d8e00263069a92
priya123dharshini <30829459+priya123dharshini@users.noreply.github.com>
2018-02-05 16:39:27 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+ int n,n1,i;
+ printf("enter an integer");
+ scanf("%d",&n);
+ for(i=0;n1!=0;i++)
+ {
+ 
+ n1=n%10;
+ if(n1%2!=0)
+ {
+   printf("%d",n1);
+  }
+  n=n/10;
+ }}
+ 

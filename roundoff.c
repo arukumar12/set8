f28fb7cfa510eb3914a03d0912b0f14f677d7a31
priya123dharshini <30829459+priya123dharshini@users.noreply.github.com>
2018-02-19 16:39:24 +0530

    Create roundoff.c

---
+++ roundoff.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(void)
+{
+  float x;
+  int con, r_value, mul;
+  printf("Please enter a value to be rounded: ");
+  scanf("%f", &x);
+  mul=x*10;
+  con=mul%10;
+  r_value=mul/10;
+  if(con>=5)
+  r_value=r_value+1;
+  else 
+  r_value=r_value;
+  printf("The rounded value is %.2d", r_value);
+
+  return 0;
+}
